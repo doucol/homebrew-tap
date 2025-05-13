4bb74327fddf0b016ab5676fe411c3d28feaca54
Doug Collier <540588+doucol@users.noreply.github.com>
2025-05-13 14:30:31 -0500

    Version 0.2.5

--- Formula/clyde.rb
+++ Formula/clyde.rb
@@ -1,28 +1,28 @@
 class Clyde < Formula
   desc "Terminal-based UI for monitoring Project Calico network flows in real-time"
   homepage "https://github.com/doucol/clyde"
-  version "0.2.4"
+  version "0.2.5"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/doucol/clyde/releases/download/v#{version}/clyde_Darwin_arm64.tar.gz"
-      sha256 "3145f5adb9850b80c4f9e699f5f7dd01b621306a3ac60e98e740427b09b4ae96"
+      sha256 "51b763e2f7b0a49de77d0fbe88f02b1034bd48e470e1888f0024b889394ab633"
     end
     on_intel do
       url "https://github.com/doucol/clyde/releases/download/v#{version}/clyde_Darwin_x86_64.tar.gz"
-      sha256 "370413c1a6654b5544186406130b4494695d9a613811a3961693fba2e3617c8a"
+      sha256 "5b67effdd5377a464fa7e9ce8b2d3caab0dc23166366d376b58f00649d889802"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/doucol/clyde/releases/download/v#{version}/clyde_Linux_arm64.tar.gz"
-      sha256 "5f2d8bab1a95bf784ac829f919298230cbf903a1d583ffe334444d2cff37f89f"
+      sha256 "89c0594a979c1c160296408ad494138ead00ea835aadce2f68502c170379fa81"
     end
     on_intel do
       url "https://github.com/doucol/clyde/releases/download/v#{version}/clyde_Linux_x86_64.tar.gz"
-      sha256 "745609c75fdb3840feb82c4f680ece00cbe2d34c7d3c041f49ee592f9a32c177"
+      sha256 "6f6fd18a36610ce1274193b7c427e8cb21d746761b789b8d8a53edcecf34d183"
     end
   end
 
