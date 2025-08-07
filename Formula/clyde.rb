80deed939365044a16f96e003b827eda0c33ef67
Doug Collier <540588+doucol@users.noreply.github.com>
2025-08-07 15:23:17 -0500

    add ver to artifact download names

--- Formula/clyde.rb
+++ Formula/clyde.rb
@@ -9,7 +9,7 @@ class Clyde < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/doucol/clyde/releases/download/v0.4.4/clyde_darwin_amd64.tar.gz", using: CurlDownloadStrategy
+      url "https://github.com/doucol/clyde/releases/download/v0.4.4/clyde_0.4.4_darwin_amd64.tar.gz", using: CurlDownloadStrategy
       sha256 "42c5e7470a87b001e5c36b2509fcb0b2629100bbe1a15fca0b6b45cd27fa0a82"
 
       def install
@@ -17,7 +17,7 @@ class Clyde < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/doucol/clyde/releases/download/v0.4.4/clyde_darwin_arm64.tar.gz", using: CurlDownloadStrategy
+      url "https://github.com/doucol/clyde/releases/download/v0.4.4/clyde_0.4.4_darwin_arm64.tar.gz", using: CurlDownloadStrategy
       sha256 "5dceae6cb7dd8d86700363aba237f4780a2da501a3adb70dc47c85bb747ba176"
 
       def install
@@ -28,14 +28,14 @@ class Clyde < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/doucol/clyde/releases/download/v0.4.4/clyde_linux_amd64.tar.gz", using: CurlDownloadStrategy
+      url "https://github.com/doucol/clyde/releases/download/v0.4.4/clyde_0.4.4_linux_amd64.tar.gz", using: CurlDownloadStrategy
       sha256 "39794a1a08f35bb7249e7db2ec723a98c5ce657cc1b43cc6d0c264680071b671"
       def install
         bin.install "clyde"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/doucol/clyde/releases/download/v0.4.4/clyde_linux_arm64.tar.gz", using: CurlDownloadStrategy
+      url "https://github.com/doucol/clyde/releases/download/v0.4.4/clyde_0.4.4_linux_arm64.tar.gz", using: CurlDownloadStrategy
       sha256 "600d369ff69aadd566d28633b983f36a8503aa774370e61f7f9debf2b80eb82a"
       def install
         bin.install "clyde"
